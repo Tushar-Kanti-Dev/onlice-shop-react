92dee0e6b5a2bfec599aae0ec93fa48ce1f5015f
Tushar-Kanti-Dev <62166306+Tushar-Kanti-Dev@users.noreply.github.com>
2023-01-29 17:03:59 +0600

    delete button update and order check button

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,25 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
+import { removeFromDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Orders = () => {
     const [products, setProducts] = useProducts();
     const [cart, setCart] = useCart(products);
+
+    const handelRemoveProduct = product =>{
+        const rest = cart.filter(pd=> pd.id !== product.id);
+        setCart(rest);
+        removeFromDb(product.id);
+    }
     return (
         <div className='shop-container'>
-            <div className="products-container">
+            <div className="review-item-container">
                 {
                     cart.map(product=> <ReviewItem
                     key={product.id}
                     product = {product}
+                    handelRemoveProduct={handelRemoveProduct}
                     ></ReviewItem>)
                 }
                 
             </div>
             <div className="cart-container">
-                <Cart cart={cart}></Cart>
+                <Cart cart={cart}>
+                    <Link to='/inventory'>
+                        <button>Proced Checkout</button>
+                    </Link>
+                </Cart>
             </div>
         </div>
     );
--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,12 +1,30 @@
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import './ReviewItem.css'
 
 const ReviewItem = (props) => {
+    const {product, handelRemoveProduct} = props
     const {name, quantity, img, price, shipping} = props.product;
     return (
-        <div>
-            <h3>this is review items: {name}</h3>
-            <h3>this is review items: {price}</h3>
-            <h3>this is review items: {quantity}</h3>
+        <div className='review-item'>
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div className="review-items-details-container">
+                <div className="review-item-details">
+                    <p className='item-name' title={name}>
+                        {name.length>20 ? name.slice(0, 20)+'...': name}
+                        </p>
+                    <p>Price: <small className='price-color'>${price}</small></p>
+                    <p>Quantity:<small className='price-color'> {quantity}</small></p>
+                </div>
+                <div className="delete-container">
+                    <button onClick={()=>handelRemoveProduct(product)} className='delete-button'>
+                    <FontAwesomeIcon className='delete-icon' icon={faTrashAlt} />
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
